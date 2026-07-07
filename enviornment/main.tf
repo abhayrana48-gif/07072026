0d876cf78b3a01d0b1e7c42093682e82760dcd91
abhayrana48-gif <abhayrana48@gmail.com>
2026-07-07 10:58:07 +0530

    Rename module variables for consistency

--- enviornment/main.tf
+++ enviornment/main.tf
@@ -1,25 +1,25 @@
 module "rg" {
   source = "../azurerm_resource_group"
-  abc = var.rgs
+  rgs = var.rgs
 }
 
 module "storage" {
     depends_on = [ module.rg ]
   source = "../azurerm_storage_account"
-  def = var.storage
+  storage = var.storage
 }
 
 
 module "vnet" {
     depends_on = [ module.storage ]
     source = "../azurerm_virtual_network"
-  ghi = var.vnets
+  vnets = var.vnets
 }
 
 
 module "subnet" {
     depends_on = [ module.vnet ]
     source = "../azurerm_subnet"
-  jkl = var.subnets
+  subnets = var.subnets
 }
 
