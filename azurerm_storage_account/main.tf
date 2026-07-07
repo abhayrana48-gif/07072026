e9b85e64774662cfaf4ce2a104f348e6cb33d7cc
abhayrana48-gif <abhayrana48@gmail.com>
2026-07-07 11:06:15 +0530

    Fix variable reference for account_tier

--- azurerm_storage_account/main.tf
+++ azurerm_storage_account/main.tf
@@ -3,6 +3,6 @@ resource "azurerm_storage_account" "this" {
   name = each.value.name
   location = each.value.location
   resource_group_name = each.value.resource_group_name
-  account_tier = each.value.access_tier
+  account_tier = each.value.account_tier
   account_replication_type = each.value.account_replication_type
 }
