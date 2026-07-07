70a2c7f425993442a3330507ed17b01d26c9e22f
abhayrana48-gif <abhayrana48@gmail.com>
2026-07-07 11:04:02 +0530

    Update main.tf

--- azurerm_resource_group/main.tf
+++ azurerm_resource_group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rgs" {
   for_each = var.rgs
-  name = var.value.name
-  location = var.value.location
+  name = each.value.name
+  location = each.value.location
 }
